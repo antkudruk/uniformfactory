e90522be3f286d894a3208bd30ac83053330fda0
Anton Kudruk <akudruk@scnsoft.com>
2020-07-06 22:04:04 +0300

    Check gitlab restrictions

--- gradle-listing-1-test/src/main/java/com/github/antkudruk/uniformfactory/test/gradleplugin/emptywrapper/Marker.java
+++ gradle-listing-1-test/src/main/java/com/github/antkudruk/uniformfactory/test/gradleplugin/emptywrapper/Marker.java
@@ -5,7 +5,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-// TODO: Remove the comment. Just checking gitlab restrictions
 // Marks classes enhanced by Wrapper
 @Retention(RetentionPolicy.CLASS)
 @Target(ElementType.TYPE)
