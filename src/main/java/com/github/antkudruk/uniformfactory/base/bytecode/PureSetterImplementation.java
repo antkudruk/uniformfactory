e0c26f517930902860cdb64a2e266a17f6404a48
Anton Kudruk <antkudruk@gmail.com>
2022-06-03 23:40:20 +0300

    Added pure setter without parameter translator

---
+++ src/main/java/com/github/antkudruk/uniformfactory/base/bytecode/PureSetterImplementation.java
@@ -0,0 +1,138 @@
+package com.github.antkudruk.uniformfactory.base.bytecode;
+
+import com.github.antkudruk.uniformfactory.common.TypeDescriptionShortcuts;
+import net.bytebuddy.description.field.FieldDescription;
+import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.scaffold.InstrumentedType;
+import net.bytebuddy.implementation.Implementation;
+import net.bytebuddy.implementation.bytecode.ByteCodeAppender;
+import net.bytebuddy.implementation.bytecode.Duplication;
+import net.bytebuddy.implementation.bytecode.StackManipulation;
+import net.bytebuddy.implementation.bytecode.assign.TypeCasting;
+import net.bytebuddy.implementation.bytecode.assign.primitive.PrimitiveUnboxingDelegate;
+import net.bytebuddy.implementation.bytecode.constant.FieldConstant;
+import net.bytebuddy.implementation.bytecode.constant.IntegerConstant;
+import net.bytebuddy.implementation.bytecode.member.FieldAccess;
+import net.bytebuddy.implementation.bytecode.member.MethodInvocation;
+import net.bytebuddy.implementation.bytecode.member.MethodReturn;
+import net.bytebuddy.implementation.bytecode.member.MethodVariableAccess;
+import net.bytebuddy.jar.asm.MethodVisitor;
+import net.bytebuddy.matcher.ElementMatchers;
+
+import java.lang.reflect.Field;
+
+public class PureSetterImplementation implements Implementation {
+
+    private final String originFieldName;
+    private final FieldDescription valueField;
+
+    public PureSetterImplementation (
+            String originFieldName,
+            FieldDescription valueField) {
+
+        this.originFieldName = originFieldName;
+        this.valueField = valueField;
+    }
+
+    @Override
+    public ByteCodeAppender appender(Implementation.Target implementationTarget) {
+
+        FieldDescription originField = implementationTarget
+                .getInstrumentedType()
+                .getDeclaredFields()
+                .filter(ElementMatchers.named(originFieldName))
+                .getOnly();
+
+        return new Appender(originField, valueField);
+    }
+
+    @Override
+    public InstrumentedType prepare(InstrumentedType instrumentedType) {
+        return instrumentedType;
+    }
+
+    public static class Appender implements ByteCodeAppender {
+        private final FieldDescription originField;
+        private final FieldDescription targetField;
+
+        Appender(FieldDescription originField,
+                 FieldDescription targetField) {
+
+            this.originField = originField;
+            this.targetField = targetField;
+        }
+
+        @Override
+        public ByteCodeAppender.Size apply(
+                MethodVisitor methodVisitor,
+                Implementation.Context implementationContext,
+                MethodDescription instrumentedMethod) {
+
+            StackManipulation stackManipulation = targetField.isPublic()
+                    ? publicFieldStackManipulation(instrumentedMethod)
+                    : privateFieldStackManipulation(instrumentedMethod);
+
+            return new Size(stackManipulation.apply(methodVisitor, implementationContext)
+                    .getMaximalSize(),
+                    instrumentedMethod.getStackSize());
+        }
+
+        private StackManipulation privateFieldStackManipulation(
+                MethodDescription instrumentedMethod) {
+            return new StackManipulation.Compound(
+                    new FieldConstant(targetField.asDefined()),
+                    // Stack: targetField,
+                    Duplication.of(new TypeDescription.ForLoadedType(Field.class)),
+                    // stack: targetField, targetField
+                    IntegerConstant.forValue(true),
+                    // stack: targetField, targetField, 'true'
+                    MethodInvocation.invoke(TypeDescriptionShortcuts.deepFindMethod(
+                            new TypeDescription.ForLoadedType(Field.class),
+                            "setAccessible",
+                            boolean.class).orElseThrow(RuntimeException::new)),
+                    // stack: targetField
+                    MethodVariableAccess.loadThis(),
+                    // Stack: targetField, this
+                    FieldAccess.forField(originField).read(),
+                    // Stack: targetField, origin
+                    MethodVariableAccess.REFERENCE.loadFrom(
+                            instrumentedMethod.getParameters().get(0).getOffset()),
+                    // Stack: targetField, origin, value
+                    TypeCasting.to(targetField.getType().asErasure().asBoxed()), // TODO: Can get rid?
+                    // Stack: targetField, origin, value
+                    MethodInvocation.invoke(TypeDescriptionShortcuts.findMethod(
+                            Field.class,
+                            "set",
+                            Object.class,
+                            Object.class).orElseThrow(RuntimeException::new)
+                    ),
+                    // Stack:
+                    MethodReturn.VOID
+            );
+        }
+
+        private StackManipulation publicFieldStackManipulation(
+                MethodDescription instrumentedMethod) {
+
+            return new StackManipulation.Compound(
+                    MethodVariableAccess.loadThis(),
+                    // Stack: this
+                    FieldAccess.forField(originField).read(),
+                    // Stack: origin, translator
+                    MethodVariableAccess.REFERENCE.loadFrom(
+                            instrumentedMethod.getParameters().get(0).getOffset()),
+                    // Stack: origin, translator, argument
+                    TypeCasting.to(targetField.getType().asErasure().asBoxed()), // TODO: Can get rid?
+                    // Stack: origin, translator, argument
+                    targetField.getType().isPrimitive()
+                            ? PrimitiveUnboxingDelegate.forPrimitive(targetField.getType())
+                            : StackManipulation.Trivial.INSTANCE,   // TODO: Test for the different types, including a custom one
+                    // Stack: origin, value
+                    FieldAccess.forField(targetField).write(),
+                    // Stack:
+                    MethodReturn.VOID
+            );
+        }
+    }
+}
---
+++ src/test/java/com/github/antkudruk/uniformfactory/base/bytecode/PureSetterImplementationTest.java
@@ -0,0 +1,101 @@
+package com.github.antkudruk.uniformfactory.base.bytecode;
+
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.description.field.FieldDescription;
+import net.bytebuddy.description.modifier.Visibility;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.implementation.FieldAccessor;
+import net.bytebuddy.implementation.Implementation;
+import net.bytebuddy.implementation.MethodCall;
+import net.bytebuddy.jar.asm.Opcodes;
+import net.bytebuddy.matcher.ElementMatchers;
+import org.junit.Test;
+import org.powermock.reflect.Whitebox;
+
+import static com.github.antkudruk.uniformfactory.common.TypeDescriptionShortcuts.findConstructor;
+import static org.junit.Assert.assertEquals;
+
+public class PureSetterImplementationTest {
+
+    private static final String ORIGIN_FIELD = "origin";
+    private static final String METHOD_NAME = "set";
+
+    @SuppressWarnings("unused")
+    public static class OriginImpl {
+
+        public String publicStringField;
+
+        private String privateStringField;
+
+        public int publicIntValue;
+
+        private int privateIntValue;
+
+        public Integer publicIntegerValue;
+
+        private Integer privateIntegerValue;
+    }
+
+    @Test
+    public void givenPublicStringField() throws Exception {
+        givenPublicIntField("publicStringField", String.class, "value");
+    }
+
+    @Test
+    public void givenPrivateStringField() throws Exception {
+        givenPublicIntField("privateStringField", String.class, "value");
+    }
+
+    @Test
+    public void givenPublicIntegerField() throws Exception {
+        givenPublicIntField("publicIntegerValue", Integer.class, 13);
+    }
+
+    @Test
+    public void givenPrivateIntegerField() throws Exception {
+        givenPublicIntField("privateIntegerValue", Integer.class, 13);
+    }
+
+    @Test
+    public void givenPublicIntField() throws Exception {
+        givenPublicIntField("publicIntValue", Integer.class, 13);
+    }
+
+    @Test
+    public void givenPrivateIntField() throws Exception {
+        givenPublicIntField("privateIntValue", Integer.class, 13);
+    }
+
+
+    private void givenPublicIntField(String fieldName, Class<?> argumentType, Object value) throws Exception {
+        // Given
+        FieldDescription.InDefinedShape valueField = new TypeDescription.ForLoadedType(OriginImpl.class)
+                .getDeclaredFields()
+                .filter(ElementMatchers.named(fieldName))
+                .getOnly();
+
+        // When
+        Implementation implementation = new PureSetterImplementation(ORIGIN_FIELD, valueField);
+
+        ByteBuddy byteBuddy = new ByteBuddy();
+        Class wrapper = byteBuddy.subclass(Object.class)
+                .defineConstructor(Visibility.PUBLIC)
+                .withParameters(OriginImpl.class)
+                .intercept(MethodCall.invoke(findConstructor(Object.class).orElse(null))
+                        .andThen(FieldAccessor.ofField(ORIGIN_FIELD).setsArgumentAt(0)))
+                .defineField(ORIGIN_FIELD, OriginImpl.class, Opcodes.ACC_PRIVATE)
+                .defineMethod(METHOD_NAME, void.class, Opcodes.ACC_PUBLIC)
+                .withParameters(argumentType)
+                .intercept(implementation)
+                .make()
+                .load(getClass().getClassLoader())
+                .getLoaded();
+
+        OriginImpl origin = new OriginImpl();
+        Object o = wrapper.getConstructor(OriginImpl.class).newInstance(origin);
+
+        // Then
+        Whitebox.invokeMethod(o, METHOD_NAME, value);
+        assertEquals(value, Whitebox.getInternalState(Whitebox.getInternalState(o, ORIGIN_FIELD), fieldName));
+    }
+}
